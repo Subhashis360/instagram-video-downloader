7c2c117dec429755f4c0ef0d1c00f485ddc888d0
Subhashis <113136062+Subhashis360@users.noreply.github.com>
2023-01-21 01:35:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let textbox = document.getElementById("textbox")
 
+var expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+var regex = new RegExp(expression);
+
 var type = "";
 
-var apikeys = ["eb44642aa4mshd1d155545aea5a4p110023jsne3281309d110","eb44642aa4mshd1d155545aea5a4p110023jsne3281309d110","eb44642aa4mshd1d155545aea5a4p110023jsne3281309d110"]
+var apikeys = ["eb44642aa4mshd1d155545aea5a4p110023jsne3281309d110","8f75f31ab0mshf76134b564a64a5p1deb25jsncec38b493eca","5fa378d3f1msh7288421b1108f32p1795e5jsnba3b200acf7b"]
 
 const apikey = apikeys[Math.floor(Math.random()*apikeys.length)]
 
@@ -12,93 +15,101 @@ let loader = document.getElementById("loading")
 
 document.getElementById("submit").addEventListener("click", () => {
         if (textbox.value != "") {
-            if (textbox.value.length > 13){
+            if (textbox.value.length > 13 ){
               show()
-              const options = {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': apikey,
-                    'X-RapidAPI-Host': 'instagram-downloader-download-instagram-videos-stories.p.rapidapi.com'
-                }
-              };
-              var url = 'https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index?url='+textbox.value;
-              fetch(url, options)
-              .then((response) => {
-                return response.json();
+              if (textbox.value.match(regex)) {
+                const options = {
+                  method: 'GET',
+                  headers: {
+                      'X-RapidAPI-Key': apikey,
+                      'X-RapidAPI-Host': 'instagram-downloader-download-instagram-videos-stories.p.rapidapi.com'
+                  }
+                };
+                var url = 'https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index?url='+textbox.value;
                 
-              })
-              .then((data) => {
-                // console.log(data.message);
-                hidee();
-                type = data.Type;
-            
-                switch (type) {
-                    case "Post-Video":
-                      type = "mp4";
-                      break;
-                    case "Story-Video":
+                fetch(url, options)
+                .then((response) => {
+                  return response.json();
+                })
+                
+                .then((data) => {
+                  console.log(data)
+                  hidee();
+                  type = data.Type;
+              
+                  switch (type) {
+                      case "Post-Video":
                         type = "mp4";
-                      break;
-                    case "Post-Image":
-                        type = "jpg";
-                      break;
-                    case "Story-Image":
-                        type = "jpg";
-                      break;
-                  }
-                  
-                  textbox.value = "";
-                  const result = Array.isArray(data.media)
-
-                  if (buttonClicked = true ) {
-                        buttonClicked = false;
-                        const list = document.getElementById("body1");
-                        var delChild = list.firstElementChild
-                        while (delChild) {
-                        list.removeChild(delChild);
-                        delChild = list.firstElementChild;
-                      }
-                  }
-                    if(result) {
-                        for(var i=0;i<data.media.length;i++){
-                            var br = document.createElement('br')
-                            var hr = document.createElement('hr')
-                            var a = document.createElement('a');
-                            let br1 = document.createElement('br')
-                
-                            var btn = document.createElement('button');
-                
-                            btn.innerHTML = `Download ${i+1}`;
-                
-                            a.appendChild(btn);
-                
-                            a.download = data.media[i];
-                
-                            a.href = data.media[i];
-                
-                            a.target = "_blank"
-                
-                            document.getElementById('body1').appendChild(br);
-                            document.getElementById('body1').appendChild(a);
-                            document.getElementById('body1').appendChild(br1)
-                            document.getElementById('body1').appendChild(hr)
+                        break;
+                      case "Story-Video":
+                          type = "mp4";
+                        break;
+                      case "Post-Image":
+                          type = "jpg";
+                        break;
+                      case "Story-Image":
+                          type = "jpg";
+                        break;
+                    }
+                    
+                    textbox.value = "";
+  
+                    const result = Array.isArray(data.media)
+  
+                    if (buttonClicked = true ) {
+                          buttonClicked = false;
+                          const list = document.getElementById("body1");
+                          var delChild = list.firstElementChild
+                          while (delChild) {
+                          list.removeChild(delChild);
+                          delChild = list.firstElementChild;
                         }
-                      } else {
-                        let br = document.createElement('br')
-                        let br1 = document.createElement('br')
-                        var a = document.createElement('a');
-                        var btn = document.createElement('button');
-                        btn.innerHTML = "Download";
-                        a.appendChild(btn);
-                        a.href = data.media;
-                        console.log(data.media)
-                        a.download = data.media;
-                        a.target = "_blank";
-                        document.getElementById('body1').appendChild(br1);
-                        document.getElementById('body1').appendChild(a);
-                        document.getElementById('body1').appendChild(br);
-                      }
-            })
+                    }
+                      if(result) {
+                          for(var i=0;i<data.media.length;i++){
+                              var br = document.createElement('br')
+                              var hr = document.createElement('hr')
+                              var a = document.createElement('a');
+                              let br1 = document.createElement('br')
+                  
+                              var btn = document.createElement('button');
+                  
+                              btn.innerHTML = `Download ${i+1}`;
+                  
+                              a.appendChild(btn);
+                  
+                              a.download = data.media[i]+"&dl=1";
+                  
+                              a.href = data.media[i]+"&dl=1";
+                  
+                              a.target = "_blank"
+                  
+                              document.getElementById('body1').appendChild(br);
+                              document.getElementById('body1').appendChild(a);
+                              document.getElementById('body1').appendChild(br1)
+                              document.getElementById('body1').appendChild(hr)
+                          }
+                        } else {
+                          let br = document.createElement('br')
+                          let br1 = document.createElement('br')
+                          var a = document.createElement('a');
+                          var btn = document.createElement('button');
+                          btn.innerHTML = "Download";
+                          a.appendChild(btn);
+                          a.href = data.media+"&dl=1";
+                          console.log(data.media)
+                          a.download = data.media+"&dl=1";
+                          a.target = "_blank";
+                          document.getElementById('body1').appendChild(br1);
+                          document.getElementById('body1').appendChild(a);
+                          document.getElementById('body1').appendChild(br);
+                        } 
+                  }) 
+                } else {
+                  hidee();
+                  alert("Please Enter A valid URL")
+              }
+
             } else {
               alert("Please Enter A valid URL")
             }
