931257efae562bad55915107a2f6d4edbe718a9b
Subhashis <113136062+Subhashis360@users.noreply.github.com>
2023-01-22 18:29:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var regex = new RegExp(expression);
 
 var type = "";
 
-var apikeys = ["eb44642aa4mshd1d155545aea5a4p110023jsne3281309d110","8f75f31ab0mshf76134b564a64a5p1deb25jsncec38b493eca","5fa378d3f1msh7288421b1108f32p1795e5jsnba3b200acf7b"]
+var apikeys = ["eb44642aa4mshd1d155545aea5a4p110023jsne3281309d110","8f75f31ab0mshf76134b564a64a5p1deb25jsncec38b493eca","5fa378d3f1msh7288421b1108f32p1795e5jsnba3b200acf7b", "7337f6bba4mshdfe5e3cb7431635p14ca7ajsn8ea74c688597"]
 
 const apikey = apikeys[Math.floor(Math.random()*apikeys.length)]
 
@@ -139,7 +139,17 @@ if (localStorage.getItem("visited") === null) {
     document.getElementById("container").style.display = "block";
     localStorage.setItem("visited", true);
   }else {
-    document.getElementById("container").style.display = "none"; 
+    let x = 0;
+    if (x === 0) {
+        x = Math.floor((Math.random() * 100) + 1)
+        if (x <= 30) {
+            document.getElementById("container").style.display = "block";
+        } else {
+            document.getElementById("container").style.display = "none";
+        }
+    } else {
+        document.getElementById("container").style.display = "none";
+    }
 }
 
 
