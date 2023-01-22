d93291b33e0fbcfea81e4dd2a6f50f95fa0bfd2b
Subhashis <113136062+Subhashis360@users.noreply.github.com>
2023-01-22 19:11:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 let textbox = document.getElementById("textbox")
-
+import { apikey } from './apikey.js';
 var expression = /^((https|http)?:\/\/(?:www\.)?instagram\.com(\/[A-Za-z0-9_@./#&+-]*)?\/(p|tv|reel|stories)\/([^/?#&]+)).*/;
 var regex = new RegExp(expression);
 
 var type = "";
-
-var apikeys = ["eb44642aa4mshd1d155545aea5a4p110023jsne3281309d110","8f75f31ab0mshf76134b564a64a5p1deb25jsncec38b493eca","5fa378d3f1msh7288421b1108f32p1795e5jsnba3b200acf7b", "7337f6bba4mshdfe5e3cb7431635p14ca7ajsn8ea74c688597"]
-
-const apikey = apikeys[Math.floor(Math.random()*apikeys.length)]
 
 var buttonClicked = false;
 
@@ -35,7 +31,6 @@ document.getElementById("submit").addEventListener("click", () => {
                   return response.json();
                 })
                 .then((data) => {
-                  console.log(data)
                   hidee();
                   type = data.Type;
                   
